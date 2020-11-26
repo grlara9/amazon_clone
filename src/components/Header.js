5dfbbee8b89c3c839451953cbc378d70c79f247e
Guillermo Lara <grlara9@gmail.com>
2020-11-25 23:03:43 -0800

    new component home and exported to app.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,6 +14,13 @@ const Header =()=>{
                 <input className="header__searchInput" type="text" />
                 <SearchIcon className="header__searchIcon" />
             </div>
+
+            <div className="header__nav">
+                <div className="header__option">
+                    <span className="header__optionLineOne">Hello Guest</span>
+                    <span className="header__optionLineTwo">Sign In</span>
+                </div>
+            </div>
         </div>
     )
 }
