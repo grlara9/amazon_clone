0c37cae694a07ec8125b15fcbbf1a80443592df8
Guillermo Lara <grlara9@gmail.com>
2020-11-29 14:15:31 -0800

    completed next row with 3 components

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,6 +4,7 @@ import './Product.css'
 const Product =({id, title, price, image})=>{
     return(
         <div className="product">
+            
             <div className="product__info">
                 <p>{title}</p>
                 <p className="product__price">
