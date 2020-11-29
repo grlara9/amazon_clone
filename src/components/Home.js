0dd51d9d3480df2d2652847eb7afe362c1399fbf
Guillermo Lara <grlara9@gmail.com>
2020-11-29 14:05:26 -0800

    enter product information and pass it through props

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,8 @@ import Product from './Product'
 import './Home.css'
 
 const Home = ()=>{
+
+   
     return(
         <div className="home">
             <div className="home__container">
@@ -13,6 +15,25 @@ const Home = ()=>{
                 />
 
                 <div className="home__row">
+                    <Product 
+                        id="12321341"
+                        title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
+                        price={11.96}
+                        rating={5}
+                        image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
+                    />
+                    <Product 
+                        id="49538094"
+                        title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
+                        price={239.0}
+                        rating={4}
+                        image="https://images-na.ssl-images-amazon.com/images/I/81O%2BGNdkzKL._AC_SX450_.jpg"
+                    />
+                </div>
+
+                <div className="home__row">
+                    <Product />
+                    <Product />
                     <Product />
                 </div>
             </div>
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import './Product.css'
 
-const Product =()=>{
+const Product =({id, title, price, image})=>{
     return(
         <div className="product">
             <div className="product__info">
-                <p>Title</p>
+                <p>{title}</p>
                 <p className="product__price">
                     <small>$</small>
-                    <strong>19.99</strong>
+                    <strong>{price}</strong>
                 </p>
                 <div className="product__rating">
                     
                 </div>
             </div>
-            <img src="" alt=""/>
+            <img src={image} alt=""/>
             <button>Add to basket</button>
         </div>
     );
