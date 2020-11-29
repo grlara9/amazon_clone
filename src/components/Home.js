1803ba5746e4ca39db1a17b340ee92e63b706a9a
Guillermo Lara <grlara9@gmail.com>
2020-11-28 21:39:18 -0800

    edit product component css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
 import React from 'react';
+
+import Product from './Product'
 import './Home.css'
 
 const Home = ()=>{
@@ -9,6 +11,10 @@ const Home = ()=>{
                     src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
                     alt=""
                 />
+
+                <div className="home__row">
+                    <Product />
+                </div>
             </div>
         </div>
     )
