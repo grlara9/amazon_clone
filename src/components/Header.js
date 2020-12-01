1ee44ae7ceec9de43402e0184c7ee2f2c02b7b98
Guillermo Lara <grlara9@gmail.com>
2020-11-30 17:40:59 -0800

    added options on navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,6 +20,22 @@ const Header =()=>{
                     <span className="header__optionLineOne">Hello Guest</span>
                     <span className="header__optionLineTwo">Sign In</span>
                 </div>
+
+                <div className="header__option">
+                    <span className="header__optionLineOne">Return</span>
+                    <span className="header__optionLineTwo">Orders</span>
+                </div>
+
+                <div className="header__option">
+                    <span className="header__optionLineOne">Your</span>
+                    <span className="header__optionLineTwo">Prime</span>
+                </div>
+
+                <div className="header__optionBasket">
+                    <ShoppingBasketIcon />
+                    <span className="header__optionLineTwo header__basketCount">0</span>
+                </div>
+
             </div>
         </div>
     )
