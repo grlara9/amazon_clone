76a7028637c4dd585d003d2f38c8fa5704ccfe3a
Guillermo Lara <grlara9@gmail.com>
2020-12-01 18:29:49 -0800

    install react router dom and added two routes home and another for checkout page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from './components/Header'
 import Home from './components/Home'
+import Checkout from './components/Checkout'
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <Router>
+        <Switch>
+      
+        <Route exact path="/">
+            <Header />
+            <Home />
+        </Route>
+        <Route path="/checkout">
+            <Checkout />
+        </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
