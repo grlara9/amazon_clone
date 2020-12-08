683c73294cc4f1901dd336554267d4bfd97c225a
Guillermo Lara <grlara9@gmail.com>
2020-12-07 22:43:52 -0800

    continue working on checkoutproduct style

--- src/components/CheckoutProduct.js
+++ src/components/CheckoutProduct.js
@@ -7,6 +7,20 @@ const CheckoutProduct = (props)=>{
             <img className="checkoutproduct__image" 
             src={props.image}
             />
+            <div className="checkoutproduct__info">
+    <p className="checkoutproduct__title">{title}</p>
+    <p className="checkoutproduct__price">
+        <small>$</small>
+        <strong>{price}</strong>
+    </p>
+    <div className="checkoutproduct__rating">
+        {Array(rating)
+        .fill().map((_, i)=>(
+            <p>*</p>
+        ))}
+    </div>
+    
+            </div>
 
         </div>
     )
