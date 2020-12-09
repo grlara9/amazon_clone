1c700dcb9dbfe5176e3945eb479f08677097b3c1
Guillermo Lara <grlara9@gmail.com>
2020-12-08 21:13:01 -0800

    modify checkoutproduct styling

--- src/components/CheckoutProduct.js
+++ src/components/CheckoutProduct.js
@@ -5,23 +5,20 @@ const CheckoutProduct = ({id, title, price, image, rating, hideButton})=>{
     return(
         <div className="checkoutproduct">
             <img className="checkoutproduct__image" src={image} />
-        <div className="checkoutproduct__info">
-            <p className="checkoutproduct__title">{title}</p>
-            <p className="checkoutproduct__price">
-                <small>$</small>
-                <strong>{price}</strong>
-            </p>
-        <div className="checkoutproduct__rating">
-            {Array(rating)
-                .fill().map((_, i)=>(
-                    <p>*</p>
-                )
-            )
-        }
-        </div>
-        <button>Remove from Basket</button>
-        </div>
-
+                <div className="checkoutproduct__info">
+                    <p className="checkoutproduct__title">{title}</p>
+                    <p className="checkoutproduct__price">
+                        <small>$</small>
+                        <strong>{price}</strong>
+                    </p>
+                <div className="checkoutproduct__rating">
+                    {Array(rating)
+                        .fill().map((_, i)=>(
+                            <p>*</p>
+                    ))}
+                </div>
+                 <button>Remove from Basket</button>
+            </div>
         </div>
     )
 }
