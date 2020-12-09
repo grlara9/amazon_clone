ab911aa7fe89b5922ea3968b0811103b95effd72
Guillermo Lara <grlara9@gmail.com>
2020-12-08 18:12:44 -0800

    started working on styling the checkoutproduct page

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -11,16 +11,16 @@ const Checkout =()=>{
     return(
         <div className="checkout">
             <div className="checkout__left">
-                <img className="checkout__ad" 
-                src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="" 
-                />
-
-                <div>
-                    <h2 className="checkout__title">your shopping basket</h2>
+                <img className="checkout__ad"  src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="" />
+            <div>
+                <h2 className="checkout__title">your shopping basket</h2>
                     {basket.map(item => (
-                        <CheckoutProduct 
-                        id={item.id}
-                        title={item.title}
+                         <CheckoutProduct 
+                            id='12345'
+                            title='arriva mis pumas bolas de vergas ptos hhoho'
+                            image='https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg'
+                            price={199.99}
+                            rating={4}
                         />
                     ))}
                 </div>
--- src/components/CheckoutProduct.js
+++ src/components/CheckoutProduct.js
@@ -1,26 +1,26 @@
 import React from 'react'
 import './CheckoutProduct.css'
 
-const CheckoutProduct = (props)=>{
+const CheckoutProduct = ({id, title, price, image, rating, hideButton})=>{
     return(
         <div className="checkoutproduct">
-            <img className="checkoutproduct__image" 
-            src={props.image}
-            />
-            <div className="checkoutproduct__info">
-    <p className="checkoutproduct__title">{title}</p>
-    <p className="checkoutproduct__price">
-        <small>$</small>
-        <strong>{price}</strong>
-    </p>
-    <div className="checkoutproduct__rating">
-        {Array(rating)
-        .fill().map((_, i)=>(
-            <p>*</p>
-        ))}
-    </div>
-    
-            </div>
+            <img className="checkoutproduct__image" src={image} />
+        <div className="checkoutproduct__info">
+            <p className="checkoutproduct__title">{title}</p>
+            <p className="checkoutproduct__price">
+                <small>$</small>
+                <strong>{price}</strong>
+            </p>
+        <div className="checkoutproduct__rating">
+            {Array(rating)
+                .fill().map((_, i)=>(
+                    <p>*</p>
+                )
+            )
+        }
+        </div>
+           
+        </div>
 
         </div>
     )
