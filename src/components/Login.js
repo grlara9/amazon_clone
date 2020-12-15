63340d349aeb77cf8925533fa9447737d9dc9fef
Guillermo Lara <grlara9@gmail.com>
2020-12-14 20:27:02 -0800

    modify styles for login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,7 +10,7 @@ const Login =()=>{
             </Link>
 
             <div className="login__container">
-                <h2>Sign In</h2>
+                <h1>Sign In</h1>
 
                 <form>
                     <h5>E-mail</h5>
@@ -18,14 +18,14 @@ const Login =()=>{
                     <h5>Password</h5>
                     <input type="password" />
 
-                    <button>Sign In</button>
+                    <button className="login__button">Sign In</button>
 
                     <p>
                     By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use & Sale. Please
                     see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
                     </p>
 
-                    <button>Create Amazon Account</button>
+                    <button className="register__button">Create Amazon Account</button>
                 </form>
             </div>
         </div>
