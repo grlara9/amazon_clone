44f63e9dfe586603c30c453591a14f687e3a5390
Guillermo Lara <grlara9@gmail.com>
2020-12-19 18:14:10 -0800

    set on app.js a listener to firebase to know if user is logged in or not

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, {useEffect} from 'react'
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from './components/Header'
@@ -6,6 +7,25 @@ import Checkout from './components/Checkout'
 import Login from './components/Login'
 
 function App() {
+
+  useEffect(() => {
+  auth.onAuthStateChange((authUser)=>{
+    console.log("The user is ::", authUser)
+
+    if(authUser){
+      dispatchEvent({
+        type: "SET_USER",
+        user: authUser,
+      });
+    } else{
+      dispatchEvent({
+        type: "SET_USER",
+        user: null
+      });
+    }
+  });
+}, [])
+
   return (
     <div className="App">
       <Router>
