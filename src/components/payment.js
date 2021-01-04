5e4944739082c92598f6f4cdceb088818d7d7c8e
Guillermo Lara <grlara9@gmail.com>
2021-01-03 19:54:20 -0800

    added review items to user can know what the user is going to buy, import checkoutproduct

--- src/components/payment.js
+++ src/components/payment.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import CheckoutProduct from './CheckoutProduct'
 import './payment.css'
+import {useStateValue} from './StateProvider'
+
 const Payment = ()=>{
+    const [{basket, user}, dispatch]= useStateValue();
+
     return(
-        <h1>Hola</h1>
+        <div className="payment">
+            <div className="payment__container">
+
+                <div className="payment__section">
+                    <div className="payment__title">
+                        <h3>Review items and deliveries</h3>
+                    </div>
+                        <div className="payment__items">
+                            {basket.map(item=>(
+                                <CheckoutProduct
+                                    id={item.id}
+                                    title={item.title}
+                                    image={item.image}
+                                    price={item.price}
+                                    rating={item.rating}
+                                />
+                            ))}
+                        </div>
+                </div>
+
+            </div>
+        </div>
     )
 }
 export default Payment
